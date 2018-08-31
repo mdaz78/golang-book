4b625ef2bd71a567c06896e42f81561b08c265bf
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-08-31 11:58:14 +0530

    Solve exercise

--- chapter6/main.go
+++ chapter6/main.go
@@ -83,6 +83,69 @@ func questionTwo(x int) (int, bool) {
 	return value, false
 }
 
+/*
+Q3 -> Write a function with one variadic parameter that finds the greatest number in a
+list of numbers.
+*/
+
+func questionThree(args ...int) int {
+	var max int
+	for i, value := range args {
+		if i == 0 || value > max {
+			max = value
+		}
+	}
+	return max
+}
+
+/*
+Q4 -> Using makeEvenGenerator as an example, write a makeOddGenerator function
+that generates odd numbers
+*/
+
+func questionFour() func() int {
+
+	i := int(1)
+	return func() (ret int) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+/*
+Q5 -> The Fibonacci sequence is defined as: fib(0) = 0, fib(1) = 1, fib(n) =
+fib(n-1) + fib(n-2). Write a recursive function that can find fib(n).
+*/
+
+func question5(x int) int {
+	if x == 0 {
+		return 1
+	}
+	return x * question5(x-1)
+}
+
+/*
+Q11 -> Write a program that can swap two integers (x := 1; y := 2; swap(&x, &y)
+should give you x=2 and y=1).
+*/
+
+func question11(x int, y int) (int, int) {
+	x, y = swap(&x, &y)
+	return x, y
+}
+
+// func swap(x *int, y *int) (int, int) {
+// 	a := x
+// 	x = y
+// 	y = a
+// 	return *x, *y
+// }
+func swap(x, y *int) (int, int) {
+	*x, *y = *y, *x
+	return *x, *y
+}
+
 func main() {
 	// x := []float64{98, 93, 77, 82, 83}
 	// y := []int{1, 2, 3, 4, 5, 6}
@@ -109,5 +172,21 @@ func main() {
 	// x := new(int)
 	// zero(x)
 	// fmt.Println(*x)
+
+	// questions
+	// 2
 	fmt.Println(questionTwo(5))
+	// 3
+	fmt.Println(questionThree(1, 2, 3, 4, 5, 6, 7))
+	// 4
+	oddGenerator := questionFour()
+	fmt.Println(oddGenerator())
+	fmt.Println(oddGenerator())
+	fmt.Println(oddGenerator())
+	fmt.Println(oddGenerator())
+	// 5
+	fmt.Println(question5(5))
+	// 11
+	fmt.Println(question11(5, 6))
+	// fmt.Println(swap(5, 6))
 }
